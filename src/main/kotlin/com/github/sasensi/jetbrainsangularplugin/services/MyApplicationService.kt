e931cb9bda78ba30fae74579c6372fc8ae809e4f
GitHub Action <action@github.com>
2022-08-02 07:25:04 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/sasensi/jetbrainsangularplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.sasensi.jetbrainsangularplugin.services
+
+import com.github.sasensi.jetbrainsangularplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/sasensi/jetbrainsangularplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/sasensi/jetbrainsangularplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.sasensi.jetbrainsangularplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sasensi.jetbrainsangularplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
